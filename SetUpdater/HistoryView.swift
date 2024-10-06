5bedcbdcb9357032af11fea1e7903746037bd65b
Kevin Guinn <kevin.p.guinn@gmail.com>
2024-10-06 11:07:08 -0400

    initial commit

---
+++ SetUpdater/HistoryView.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryView.swift
+//  SetUpdater
+//
+//  Created by Kevin Guinn on 9/17/24.
+//
+
+import Foundation
---
+++ SetUpdater/MapView.swift
@@ -0,0 +1,8 @@
+//
+//  MapView.swift
+//  SetUpdater
+//
+//  Created by Kevin Guinn on 9/17/24.
+//
+
+import Foundation
